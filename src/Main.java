d87b6f8cb0c95c867bd84b9b95eda1f3e1bad003
Roman <filinroman79@gmail.com>
2024-11-25 01:10:25 +0300

    HW12.1

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
         Book svadba = new Book("Свадьба", chehov, 1889);
         Book masterAndMargarita = new Book("Мастер и Маргарита", bulgakov, 1967);
 
+
         System.out.println(svadba);
         System.out.println(masterAndMargarita);
 
